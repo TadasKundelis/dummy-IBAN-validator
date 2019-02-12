ff03aba5c86e625cca289db8287d1b98e644f2ae
Kundelis, Tadas <tkundelis@dxc.com>
2019-02-12 23:11:06 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IBAN_validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is IBAN validator");
+            App();
+        }
+
+        static void App()
+        {
+            
+            Console.WriteLine("\nType F To validate a file or S to validate a single code:");
+            string choice = Console.ReadLine();
+            if (choice.ToLower() == "f")
+            {
+                Console.WriteLine("Please enter the path to file location:");
+                string inputDirectory = Console.ReadLine();
+                Console.WriteLine("Please enter the file name:");
+                string inputFileName = Console.ReadLine();
+                string inputPath = $"{inputDirectory}\\{inputFileName}";
+                string outputFileName = null;
+                string outputPath = null;
+                try
+                {
+                    outputFileName = Path.GetFileNameWithoutExtension(inputPath);
+                    outputPath = $"{inputDirectory}\\{outputFileName}.out";
+
+                } catch(Exception exception)
+                {
+                    Console.WriteLine($"\n{exception.Message}");
+                }
+                
+                if (File.Exists(inputPath))
+                {
+                    using (StreamWriter fileWriter = new StreamWriter(outputPath))
+                    {
+                        using (StreamReader fileReader = new StreamReader(inputPath))
+                        {
+                            while (fileReader.Peek() != -1)
+                            {
+                                string code = fileReader.ReadLine();
+                                string output = String.Format("{0};{1}", code, Validator.Run(code));
+                                fileWriter.WriteLine(output);
+                            }
+                        }
+                    }
+                    Console.WriteLine($"\nDone, output was written to {outputPath}");
+                }
+                else
+                {
+                    Console.WriteLine("\nFile not found! Please try again.");
+                }
+            }
+            else if (choice.ToLower() == "s")
+            {
+                Console.WriteLine("Please enter the code:");
+                string code = Console.ReadLine();
+                Console.WriteLine("\n" + Validator.Run(code));
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid selection. Please try again.");
+            }
+            App();
+        }
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace IBAN_validator
+{
+    public static class Validator
+    {
+        public static string Run(string code)
+        {
+            if(code.Length < 4 || code.Length > 34)
+            {
+                return "invalid";
+            };
+            Dictionary<char, string> letterToNumber = new Dictionary<char, string>
+            {
+                {'A', "10"},
+                {'B', "11"},
+                {'C', "12"},
+                {'D', "13"},
+                {'E', "14"},
+                {'F', "15"},
+                {'G', "16"},
+                {'H', "17"},
+                {'I', "18"},
+                {'J', "19"},
+                {'K', "20"},
+                {'L', "21"},
+                {'M', "22"},
+                {'N', "23"},
+                {'O', "24"},
+                {'P', "25"},
+                {'Q', "26"},
+                {'R', "27"},
+                {'S', "28"},
+                {'T', "29"},
+                {'U', "30"},
+                {'V', "31"},
+                {'W', "32"},
+                {'X', "33"},
+                {'Y', "34"},
+                {'Z', "35"}
+            };
+
+            string rearranged = code.Substring(4, code.Length - 4) + code.Substring(0, 4);
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            // loop over rearranged string and convert letters to numbers using letterToNumber dictionary
+            for (int i = 0; i < code.Length; i++)
+            {
+                char character = rearranged[i];
+                if (Char.IsLetter(character))
+                {
+                    if (!letterToNumber.ContainsKey(character))
+                    {
+                        return "invalid";
+                    }
+                    stringBuilder.Append(letterToNumber[character]);
+                }
+                else if(Char.IsDigit(character))
+                {
+                    stringBuilder.Append(character);
+                } else
+                {
+                    return "invalid";
+                }
+            }
+            //trim leading zeros
+            string trimmedString = stringBuilder.ToString().TrimStart(new Char[] { '0' });
+
+            //convert to bigint
+            BigInteger convertedToInt = 0;
+            try
+            {
+                convertedToInt = BigInteger.Parse(trimmedString);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"\n{exception.Message}");
+            };
+
+            if (BigInteger.Remainder(convertedToInt, 97) == 1)
+            {
+                return "valid";
+            }
+            else
+            {
+                return "invalid";
+            }
+        }
+    }
+}
